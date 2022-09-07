c81fe3e2ed597b67f6a57625dd525f5fe67c09be
Yashodhan Joshi <yjdoc2@gmail.com>
2022-09-07 22:56:49 +0530

    corresponding to 708730f

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,23 @@
 
 # This script is very specific to my usecase, so not much generic or anything
 
-genkiGrammer
-
 root=$(git rev-parse --show-toplevel)
-cd root
+shortSha=$(git rev-parse --short HEAD)
+
+cd $root
 
 rm -rf /tmp/gh-pages
 
 git clone git@github.com:YJDoc2/jpn-prompt-generator.git --branch gh-pages /tmp/gh-pages 
 
-cp -Lfr ./web //tmp/gh-pages
+rm -rf /tmp/gh-pages/*
+
+cp -Lfr ./web/* /tmp/gh-pages
+
+cd /tmp/gh-pages
+
+git add .
+
+git commit -m "corresponding to $shortSha"
+
+git push origin gh-pages
