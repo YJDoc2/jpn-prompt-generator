136fdc6f19ee51c2d2200ec0e1010e69e55f9d05
Yashodhan Joshi <yjdoc2@gmail.com>
2022-11-29 14:54:54 +0530

    Add formatting to all html js and css files

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -9,49 +9,49 @@ const hashSign = '%23'
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 
-async function fetchWords(levelStr){
-    let apiUrl = apiBaseUrl+keywordParam+hashSign+levelStr;
+async function fetchWords(levelStr) {
+    let apiUrl = apiBaseUrl + keywordParam + hashSign + levelStr;
     let completeData = [];
     let pageCtr = 1;
 
     console.debug(`starting data fetch for level ${levelStr}`);
 
-    while(true){
+    while (true) {
         console.debug(`fetching page ${pageCtr} for level ${levelStr}...`);
         let res = await fetch(`${apiUrl}&page=${pageCtr}`);
         let data = await res.json();
-        if(data.meta.status !== 200){
+        if (data.meta.status !== 200) {
             console.error('Unexpected error in fetching data...');
             console.error(data);
             process.exit(1);
         }
 
-        if(data.data.length <1){
+        if (data.data.length < 1) {
             break;
         }
         console.debug(`fetch for leve ${levelStr} page ${pageCtr} completed...`);
-        for(let datum of data.data){
+        for (let datum of data.data) {
             completeData.push(datum);
         }
         pageCtr += 1;
     }
     return completeData;
-    
+
 }
 
-async function fetchAll(){
+async function fetchAll() {
     // only fetches n5 and n4 for now
-    
-    
+
+
     let n5Words = await fetchWords('jlpt-n5');
-    
+
     // actual save objects
     let n5 = {
-        fetchDate:Date.now(),
-        data:n5Words
+        fetchDate: Date.now(),
+        data: n5Words
     };
 
-    fs.writeFileSync(path.join(dataDirPath,'n5.json'),JSON.stringify(n5));
+    fs.writeFileSync(path.join(dataDirPath, 'n5.json'), JSON.stringify(n5));
 
     // sleep for 5 seconds, so the api does not get overwhelmed
     console.debug('sleeping for a bit');
@@ -59,15 +59,15 @@ async function fetchAll(){
     console.debug('woke up, continuing');
     let n4Words = await fetchWords('jlpt-n4');
     let n4 = {
-        fetchDate:Date.now(),
-        data:n4Words
+        fetchDate: Date.now(),
+        data: n4Words
     };
-    fs.writeFileSync(path.join(dataDirPath,'n4.json'),JSON.stringify(n4));
+    fs.writeFileSync(path.join(dataDirPath, 'n4.json'), JSON.stringify(n4));
 
 }
 
 
-if(!process.argv[1]){
+if (!process.argv[1]) {
     console.error('Please give data directory path...');
     console.error('Syntax : node index /data/dir/path');
     process.exit(1);
@@ -75,32 +75,32 @@ if(!process.argv[1]){
 
 const dataDirPath = path.resolve(process.argv[2]);
 
-if(!fs.existsSync(dataDirPath)){
+if (!fs.existsSync(dataDirPath)) {
     console.error(`Given path of data directory ${dataDirPath} does not exist...`);
     console.error('Please give correct path');
     process.exit(1);
 }
 
 
-const dataBackupDir = path.join(dataDirPath,'backup');
+const dataBackupDir = path.join(dataDirPath, 'backup');
 
-if(fs.existsSync(dataBackupDir)){
+if (fs.existsSync(dataBackupDir)) {
     console.debug(`removing existing data backup directory : ${dataBackupDir}`);
-    fs.rmSync(dataBackupDir,{recursive:true});
+    fs.rmSync(dataBackupDir, { recursive: true });
 }
 
 console.debug('moving existing .json content of data into backup directory');
 fs.mkdirSync(dataBackupDir);
 
-for(let file of fs.readdirSync(dataDirPath)){
+for (let file of fs.readdirSync(dataDirPath)) {
     let ext = path.extname(file);
-    if(ext !== '.json'){
+    if (ext !== '.json') {
         console.debug(`skipping ${file} , as not json`);
         continue;
     }
-    let src = path.join(dataDirPath,file);
-    let dest = path.join(dataBackupDir,file);
-    fs.copyFileSync(src,dest);
+    let src = path.join(dataDirPath, file);
+    let dest = path.join(dataBackupDir, file);
+    fs.copyFileSync(src, dest);
     console.debug(`file ${file} copy complete`);
 }
 
@@ -108,6 +108,6 @@ console.debug('backup complete');
 
 console.log('Starting data fetching using jisho api...');
 
-fetchAll().then(()=>{
+fetchAll().then(() => {
     console.log('Data fetch complete...');
 })
--- scripts/filterData.js
+++ scripts/filterData.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const jishoBaseLink = 'https://jisho.org/search/';
 
-if(!process.argv[1]){
+if (!process.argv[1]) {
     console.error('Please give data directory path...');
     console.error('Syntax : node index /data/dir/path');
     process.exit(1);
@@ -11,7 +11,7 @@ if(!process.argv[1]){
 
 const dataDirPath = path.resolve(process.argv[2]);
 
-if(!fs.existsSync(dataDirPath)){
+if (!fs.existsSync(dataDirPath)) {
     console.error(`Given path of data directory ${dataDirPath} does not exist...`);
     console.error('Please give correct path');
     process.exit(1);
@@ -21,18 +21,18 @@ console.log('starting ...');
 
 // need a better way of handling this, but should work for now
 
-const n5path = path.join(dataDirPath,'n5.json');
-const n4path = path.join(dataDirPath,'n4.json');
+const n5path = path.join(dataDirPath, 'n5.json');
+const n4path = path.join(dataDirPath, 'n4.json');
 
 const n5 = require(n5path);
 const n4 = require(n4path);
 
-if(!n5){
+if (!n5) {
     console.error(`data file n5.json not found at ${n5path}`);
     process.exit(1);
 }
 
-if(!n4){
+if (!n4) {
     console.error(`data file n4.json not found at ${n4path}`);
     process.exit(1);
 }
@@ -41,11 +41,11 @@ console.debug('starting filtering n5...');
 
 let n5Filtered = [];
 
-for(let word of n5.data){
+for (let word of n5.data) {
     n5Filtered.push({
-        text:word.slug,
-        jishoLink:`${jishoBaseLink}${word.slug}`,
-        attribution:word.attribution
+        text: word.slug,
+        jishoLink: `${jishoBaseLink}${word.slug}`,
+        attribution: word.attribution
     });
 }
 
@@ -54,19 +54,19 @@ console.debug('n5 filtering complte');
 console.debug('starting filtering n4...');
 let n4Filtered = [];
 
-for(let word of n4.data){
+for (let word of n4.data) {
     n4Filtered.push({
-        text:word.slug,
-        jishoLink:`${jishoBaseLink}${word.slug}`,
-        attribution:word.attribution
+        text: word.slug,
+        jishoLink: `${jishoBaseLink}${word.slug}`,
+        attribution: word.attribution
     });
 }
 
 console.debug('n4 filtering complte');
 
 console.debug('writing data to files');
-fs.writeFileSync(path.join(dataDirPath,'n5Filtered.json'),JSON.stringify(n5Filtered));
-fs.writeFileSync(path.join(dataDirPath,'n4Filtered.json'),JSON.stringify(n4Filtered));
+fs.writeFileSync(path.join(dataDirPath, 'n5Filtered.json'), JSON.stringify(n5Filtered));
+fs.writeFileSync(path.join(dataDirPath, 'n4Filtered.json'), JSON.stringify(n4Filtered));
 console.debug('data writing complete');
 
 console.log('filtering complete');
